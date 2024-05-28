efda2107640ec85d25868db7cbfa82d512652063
Thiago Domimgos <miniruivo@gmail.com>
2024-05-28 15:58:50 -0300

    m

--- src/config/di.ts
+++ src/config/di.ts
@@ -1,7 +1,7 @@
 import Container from "typedi";
 import { getCustomRepository } from "typeorm";
 
-import { StorageService } from "../api/v1/services/../interfaces/storage.service";
+import { StorageService } from "../api/v1/interfaces/storage.service";
 import { DiskStorageService } from "../api/v1/services/disk-storage";
 import { Storage } from "./storage";
 import { S3StorageService } from "../api/v1/services/s3-storage";
