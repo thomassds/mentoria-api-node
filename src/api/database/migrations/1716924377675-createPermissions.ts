713e03585d5cbfb883b260662ccd0fbae92700c9
Thomas <thomasdomingos@hotmail.com>
2024-05-28 16:26:56 -0300

    m

---
+++ src/api/database/migrations/1716924377675-createPermissions.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createPermissions1716924377675 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
